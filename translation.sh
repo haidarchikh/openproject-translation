1dddbf7621c3981605fd86bda780e692414836b1
Haidar Chikh <haidarchikh@gmail.com>
2017-04-18 14:47:51 +0200

    copy script

---
+++ translation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Master
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml
+
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml
+
+# backlogs
+wget -O /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-backlogs/config/locales/js-xa.yml
+wget -O /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-backlogs/config/locales/xa.yml
+
+# costs
+wget -O /usr/src/app/vendored-plugins/openproject-costs/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-costs/config/locales/js-xa.yml
+wget -O /usr/src/app/vendored-plugins/openproject-costs/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-costs/config/locales/xa.yml
+
+# documents
+wget -O /usr/src/app/vendored-plugins/openproject-documents/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-documents/config/locales/xa.yml
+
+# roles
+wget -O /usr/src/app/vendored-plugins/openproject-global_roles/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-global_roles/config/locales/xa.yml
+
+# meeting
+wget -O /usr/src/app/vendored-plugins/openproject-meeting/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-meeting/config/locales/xa.yml
+
+# project page
+wget -O /usr/src/app/vendored-plugins/openproject-my_project_page/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-my_project_page/config/locales/xa.yml
+
+# pdf export
+wget -O /usr/src/app/vendored-plugins/openproject-pdf_export/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-pdf_export/config/locales/xa.yml
+
+# reporting
+wget -O /usr/src/app/vendored-plugins/openproject-reporting/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-reporting/config/locales/xa.yml
+
+# engine
+wget -O /usr/src/app/vendored-plugins/reporting_engine/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/reporting_engine/config/locales/js-xa.yml
+wget -O /usr/src/app/vendored-plugins/reporting_engine/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/reporting_engine/config/locales/xa.yml
