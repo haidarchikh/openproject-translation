6d8e4d11d23adcd9de320f90b341391ebcfab87d
Haidar Chikh <haidarchikh@gmail.com>
2017-04-18 17:12:57 +0200

    change the plans

--- translation.sh
+++ translation.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 # Master
+rm /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml
+
+rm /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/sv-SE.yml
 wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/sv-SE.yml
 
-wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml
+# backlogs
+rm /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/js-sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-backlogs/config/locales/js-sv-SE.yml
+rm /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-backlogs/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-backlogs/config/locales/sv-SE.yml
+
+# costs
+rm /usr/src/app/vendored-plugins/openproject-costs/config/locales/js-sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-costs/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-costs/config/locales/js-sv-SE.yml
+rm /usr/src/app/vendored-plugins/openproject-costs/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-costs/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-costs/config/locales/sv-SE.yml
+
+# documents
+rm /usr/src/app/vendored-plugins/openproject-documents/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-documents/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-documents/config/locales/sv-SE.yml
+
+# roles
+rm /usr/src/app/vendored-plugins/openproject-global_roles/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-global_roles/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-global_roles/config/locales/sv-SE.yml
+
+# meeting
+rm /usr/src/app/vendored-plugins/openproject-meeting/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-meeting/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-meeting/config/locales/sv-SE.yml
+
+# project page
+rm /usr/src/app/vendored-plugins/openproject-my_project_page/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-my_project_page/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-my_project_page/config/locales/sv-SE.yml
+
+# pdf export
+rm /usr/src/app/vendored-plugins/openproject-pdf_export/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-pdf_export/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-pdf_export/config/locales/sv-SE.yml
+
+# reporting
+rm /usr/src/app/vendored-plugins/openproject-reporting/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/openproject-reporting/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/openproject-reporting/config/locales/sv-SE.yml
+
+# engine
+rm /usr/src/app/vendored-plugins/reporting_engine/config/locales/js-sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/reporting_engine/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/reporting_engine/config/locales/js-sv-SE.yml
+rm /usr/src/app/vendored-plugins/reporting_engine/config/locales/sv-SE.yml
+wget -O /usr/src/app/vendored-plugins/reporting_engine/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/vendored-plugins/reporting_engine/config/locales/sv-SE.yml
+
+# js
+rm /usr/src/app/app/assets/javascripts/locales/sv-SE.js
+wget -O /usr/src/app/app/assets/javascripts/locales/sv-SE.js https://github.com/haidarchikh/openproject-translation/raw/master/usr/src/app/app/assets/javascripts/locales/sv-SE.js
