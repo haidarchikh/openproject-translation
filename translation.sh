f1f48a26ab45b6c3381812772cc8db1a40279dbe
Haidar Chikh <haidarchikh@gmail.com>
2017-04-18 16:41:25 +0200

    using old files

--- translation.sh
+++ translation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Master
-wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/sv-SE.yml
 
-wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-sv-SE.yml
---
+++ translationUsingOldFiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Master
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/js-xa.yml
+
+wget -O /usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml https://github.com/haidarchikh/openproject-translation/raw/master/usr/local/bundle/bundler/gems/openproject-translations-9821ade5d966/config/locales/xa.yml
